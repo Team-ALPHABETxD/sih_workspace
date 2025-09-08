cfa74354086b36c95dc8a499c4afe03e9387e53b
soumyo-jeet <samajdarsoumyajeet0@gmail.com>
2025-09-09 01:23:22 +0530

    SHAPified analysis added to ML

--- prediction_server/api/main.py
+++ prediction_server/api/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
 import joblib
+import shap
 
 app = Flask(__name__)
 
@@ -12,22 +13,9 @@ def predict_future():
         data = request.get_json()
         print(f"data: {data}")
         
-        # extract the feilds
-        lat = data.get('lat')
-        lon = data.get('lon')
-        yr = data.get('year')
-        state = data.get('state')
-        
-        # create sample to predict
-        sample = {
-        'lat': lat,
-        'lon': lon,
-        'year': yr,
-        'state': state
-        }
-        
         # dataframe
-        sample_df = pd.DataFrame([sample])
+        sample_df = pd.DataFrame([data])
+        print(sample_df.columns)
         
         # model fetch
         model = joblib.load('../model/future_predict_model.joblib')
@@ -39,13 +27,32 @@ def predict_future():
         # Reshape back: (5 years, n_states)
         pred_probs = preds.reshape(6, 3)
         
-        
+        # SHAP analysis
+        try:  
+            output_idx = 3  # e.g., 1-year ahead, Moderate state
+            estimator = model.estimators_[output_idx]
+            explainer = shap.TreeExplainer(estimator)
+            shap_vals = explainer.shap_values(sample_df)
+            
+            shap_dict = {
+                feat: float(round(shap_vals[0,i], 3)) for i, feat in enumerate(sample_df.columns)
+            }
+            
+            print(f"shap: {shap_dict}")
+            
+        except:
+            print("SHAP error")
+            
         # response send
         return jsonify({
-            'prediction': pred_probs.tolist()
+            'prediction': pred_probs.tolist(),
+            'shap': shap_dict
         })
     except:
         print("Something went wrong")
+        return jsonify({
+            'error': "Server not working",
+        })
 
 if __name__ == '__main__':
     app.run(debug = True)
