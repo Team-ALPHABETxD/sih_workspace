fdd9f6154b00b654e0101634ca67f313c0e2c1bd
Anjali Ray <rayyyanjali@gmail.com>
2025-09-08 23:09:24 +0530

    Dashboard dark theme added

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -10,34 +10,34 @@ import {
 import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 
-export function SidebarDemo() {
+export default function SidebarDemo() {
   const links = [
     {
       label: "Generate Report",
       href: "#",
       icon: (
-        <IconBrandTabler className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+        <IconBrandTabler className="h-5 w-5 shrink-0 text-neutral-100" />
       ),
     },
     {
       label: "Recents",
       href: "#",
       icon: (
-        <IconUserBolt className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+        <IconUserBolt className="h-5 w-5 shrink-0 text-neutral-200" />
       ),
     },
     {
       label: "Profile",
       href: "#",
       icon: (
-        <IconSettings className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+        <IconSettings className="h-5 w-5 shrink-0 text-neutral-200" />
       ),
     },
     {
       label: "Logout",
       href: "#",
       icon: (
-        <IconArrowLeft className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+        <IconArrowLeft className="h-5 w-5 shrink-0 text-neutral-200" />
       ),
     },
   ];
@@ -45,7 +45,7 @@ export function SidebarDemo() {
   return (
     <div
       className={cn(
-        "flex w-full flex-1 flex-col overflow-hidden bg-gray-100 md:flex-row dark:bg-neutral-800",
+        "flex w-full flex-1 flex-col overflow-hidden bg-neutral-800 md:flex-row",
         "h-screen", // for your use case, use `h-screen` instead of `h-[60vh]`
       )}
     >
@@ -86,13 +86,13 @@ export const Logo = () => {
   return (
     <a
       href="#"
-      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-white"
     >
-      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-white" />
       <motion.span
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className="font-medium whitespace-pre text-black dark:text-white"
+        className="font-medium whitespace-pre text-white"
       >
         Metal Craft
       </motion.span>
@@ -103,9 +103,9 @@ export const LogoIcon = () => {
   return (
     <a
       href="#"
-      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-white"
     >
-      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-white" />
     </a>
   );
 };
@@ -114,12 +114,12 @@ export const LogoIcon = () => {
 const Dashboard = () => {
   return (
     <div className="flex flex-1">
-      <div className="flex h-full w-full flex-1 flex-col gap-2 rounded-tl-2xl border border-neutral-200 bg-white p-2 md:p-10 dark:border-neutral-700 dark:bg-neutral-900">
+      <div className="flex h-full w-full flex-1 flex-col gap-2 rounded-tl-2xl border border-neutral-700 bg-neutral-900 p-2 md:p-10">
         <div className="flex gap-2">
           {[...new Array(4)].map((i, idx) => (
             <div
               key={"first-array-demo-1" + idx}
-              className="h-20 w-full animate-pulse rounded-lg bg-gray-100 dark:bg-neutral-800"
+              className="h-20 w-full animate-pulse rounded-lg bg-neutral-800"
             ></div>
           ))}
         </div>
@@ -127,7 +127,7 @@ const Dashboard = () => {
           {[...new Array(2)].map((i, idx) => (
             <div
               key={"second-array-demo-1" + idx}
-              className="h-full w-full animate-pulse rounded-lg bg-gray-100 dark:bg-neutral-800"
+              className="h-full w-full animate-pulse rounded-lg bg-neutral-800"
             ></div>
           ))}
         </div>
@@ -135,3 +135,4 @@ const Dashboard = () => {
     </div>
   );
 }
+// export default SidebarDemo;
