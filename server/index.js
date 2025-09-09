e6b6103e56d168a91c30a69c022422a07bfac3c1
Anjali Ray <rayyyanjali@gmail.com>
2025-09-10 00:53:20 +0530

    Cors added to server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const connectToMongo = require('./db')
-
+const cors = require('cors')
 const app = express()
 const PORT = 8000
 
 app.use(express.json())
+app.use(cors({ 
+    origin: 'http://localhost:3000',
+    credentials: true,
+ }))
 
 connectToMongo()
 
